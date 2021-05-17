d654187736e11effdc8c89bf494ce8e602b427ba
Farkhod Allamuradov <farkhod5allamuradov@gmail.com>
2021-05-17 16:03:24 +0500

    Initial commit

--- app.js
+++ app.js
@@ -1,105 +1,123 @@
+const players = [             
+    {
+        name: "Farhod",
+        score: 50
+    },
+    {
+        name: "Bahrom",
+        score: 85
+    },
+    {
+        name: "Eldor",
+        score: 95
+    },
+    {
+        name: "Odil",
+        score: 80
+    },
+    {
+        name: "Bekzod",
+        score: 45
+    }
+];
+
 const Header = (props) => {
-  return (
-    <header>
-      <span className="stats">Players: {props.totalPlayers}</span> 
-      <h1>{ props.title }</h1>
-    </header>
-  );
-}
-
-class Counter extends React.Component {
-  
-  state = { 
-    score: 100 
-  };
-  
-  incrementScore() {
-    this.setState( prevState => ({ 
-      score: prevState.score + 1 
-    }));
-  }
-  
-  decrementScore() {
-    this.setState( prevState => ({ 
-      score: prevState.score - 1 
-    }));
-  }
-
-  render() {
     return (
-      <div className="counter">
-        <button className="counter-action decrement" onClick={() => this.decrementScore()}> - </button>
-        <span className="counter-score"> {this.state.score} </span>
-        <button className="counter-action increment" onClick={() => this.incrementScore()}> + </button>
-      </div>    
+        <header>
+            <h1>{ props.title }</h1>
+            <span className="stats">Players: { props.totalPlayers }</span>
+        </header>
     );
-  }
-}
-  
-const Player= (props) => {
-  return (
-    <div className="player">
-      <span className="player-name">
-        <button className="remove-player" onClick={() => props.removePlayer(props.id)}>✖</button>
-        { props.name }
-      </span>
-
-      <Counter />
-    </div>
-  );
 }
 
-class App extends React.Component {
-  
-  state = {
-    players: [
-      {
-        name: "Guil",
-        id: 1,
-      },
-      {
-        name: "Treasure",
-        id: 2,    
-      },
-      {
-        name: "Ashley",
-        id: 3,    
-      },
-      {
-        name: "James",
-        id: 4,    
-      }
-    ]
-  };
-        
-  handleRemovePlayer = (id) => {
-    this.setState( prevState => {
-      return {
-        players: prevState.players.filter(p => p.id !== id)
-      }
-    });
-  }
-  
-  render() {
+const Player = (props) => {
     return (
-      <div className="scoreboard">
-        <Header 
-          title="My Scoreboard" 
-          totalPlayers={this.state.players.length} 
-        />
-        
-        {/* Players list */}
-        {this.state.players.map( player =>
-          <Player 
-            removePlayer={ this.handleRemovePlayer }      
-            name={player.name}
-            id={player.id}
-            key={player.id.toString()}
-          />
-        )}  
-      </div>
-    );
-  }
-}  
+        <div className="player">
+            <span className="player-name">
+                { props.name }
+            </span>
 
-ReactDOM.render(<App />, document.getElementById('root'));
+            <Counter score={props.score} />
+        </div>
+    );
+}
+
+const Counter = (props) => {
+    return (
+        <div className="counter">
+            <button className="counter-action decrement"> - </button>
+            <span className="counter-score">{ props.score }</span>
+            <button className="counter-action increment"> + </button>
+        </div>
+    );
+}
+
+const App = (props) => {
+    return (
+        <div className="scoreboard">
+            <Header title="Scoreboard" totalPlayers={props.initialPlayers.length} />
+
+            {/* Player list */}
+            {props.initialPlayers.map(player => 
+                <Player 
+                    name={player.name} 
+                    score={player.score} 
+                />    
+            )}
+        </div>
+    );
+}
+
+ReactDOM.render(
+    <App initialPlayers={players} />,
+    document.getElementById('root')
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const desc = 'I just learned how to create a React node and render it into the DOM.';
+// const myTitleID = 'main-title';
+// const name = 'Farhod';
+
+// const header = (
+//     <header>
+//         <h1 id={myTitleID}>{ name }'s First React Element!</h1>
+//         <p>{ desc }</p>
+//     </header>
+// );
+ 
+
+// ReactDOM.render(
+//     header,
+//     document.getElementById('root')
+// );
+
+// https://babeljs.io/repl
